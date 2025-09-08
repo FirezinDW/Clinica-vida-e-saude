997a828ca3814581128552f4efc05b0273ab7d3b
Maria Eduarda Conceição de Sá <maria58346756@edu.df.senac.br>
2025-09-08 15:24:53 -0300

    medico

---
+++ app/models/consulta.py
@@ -0,0 +1,8 @@
+class consulta:
+    def __init__(self, id, paciente_id, medico_id, data, hora, status):
+        self.id = id 
+        self.paciente_id = paciente_id
+        self.medico_id = medico_id
+        self.data = data 
+        self.status = status
+        
---
+++ app/models/medico.py
@@ -0,0 +1,11 @@
+class medico:
+    def __init__(self, id, nome, crm, especialidade, horario_atendimento ):
+        self.id = id
+        self.nome = nome
+        self.crm = crm
+        self.especialidade = especialidade
+        self.horario_atendimento = horario_atendimento
+        
+
+ 
+ 
